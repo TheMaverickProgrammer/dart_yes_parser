f2cc03fce71defcbc3fcb42ff71b47b9cca4f0a3
TheMaverickProgrammer <91709+TheMaverickProgrammer@users.noreply.github.com>
2024-10-26 17:24:35 -0400

    Multiline and custom string literal begin-and-end characters successfully implemented. All tests pass. TODO: new steps need minor optimization. Further testing to be sure new features behave as expected would be idea.

--- lib/src/element_parser.dart
+++ lib/src/element_parser.dart
@@ -5,6 +5,19 @@ import 'package:yes_parser/src/enums.dart';
 import 'package:yes_parser/src/element.dart';
 import 'package:yes_parser/src/literal.dart';
 
+/// This is a structure to assist [ElementParser.evaluateKeyVals].
+class TokenWalkInfo {
+  /// This is the string to be evaluated into a valid [KeyVal] pair.
+  final String data;
+
+  /// If +(non-zero), this is the [data] index of the [Glyphs.equal] symbol.
+  final int pivot;
+
+  bool get hasPivot => pivot >= 0;
+
+  TokenWalkInfo({required this.data, required this.pivot});
+}
+
 /// [ElementParser] parses the [elementInfo] from a line.
 /// Used internally.
 class ElementParser {
@@ -68,7 +81,7 @@ class ElementParser {
         break;
       }
 
-      // Step 2: if the first valid character is reserved prefix
+      // Step 2: If the first valid character is reserved prefix
       // then tag the element and continue searching for the name start pos
       final Glyphs glyph = Glyphs.values[idx];
       switch (glyph) {
@@ -102,7 +115,7 @@ class ElementParser {
       break;
     }
 
-    // Step 3: find end of element name (first space or EOL)
+    // Step 3: Find end of element name (first space or EOL)
     pos = min(pos, len);
     final int idx = line.indexOf(Glyphs.space.char, pos);
 
@@ -139,26 +152,12 @@ class ElementParser {
         _element = Element.standard(name);
     }
 
-    // Step 4: parse tokens, if any and return results
+    // Step 4: Parse tokens, if any and return results
     parseTokens(line, end, literals: literals);
   }
 
   void parseTokens(String input, int start, {List<Literal>? literals}) {
-    int end = start;
-
-    // Evaluate all tokens on line
-    while (end < input.length) {
-      end = parseTokenStep(input, end + 1, literals: literals);
-
-      // Abort early if there is a problem
-      if (!isOk) {
-        return;
-      }
-    }
-  }
-
-  int parseTokenStep(String input, int start, {List<Literal>? literals}) {
-    final int len = input.length;
+    int len = input.length;
 
     // Find first non-space character
     while (start < len) {
@@ -171,16 +170,21 @@ class ElementParser {
       break;
     }
 
+    // Edge case: no KeyVal args.
     if (start >= len) {
-      return len;
+      return;
     }
 
-    final int end = evaluateDelimiter(input, start, literals: literals);
-    evaluateToken(input, start, end);
-    return end;
+    /// Collect and then evaluate [KeyVal] args.
+    evaluateKeyVals(collectTokens(input, start, literals: literals));
   }
 
-  int evaluateDelimiter(String input, int start, {List<Literal>? literals}) {
+  /// Returns a list of potential [KeyVal]s from the [input] string.
+  List<TokenWalkInfo> collectTokens(
+    String input,
+    int start, {
+    List<Literal>? literals,
+  }) {
     /// User Defined Literals
     final Map<Literal, int> udLiterals = switch (literals) {
       List<Literal> literals => <Literal, int>{
@@ -192,14 +196,17 @@ class ElementParser {
     final int len = input.length;
     int current = start;
 
-    // TODO: if this change works, reduce the step numbers
-    // Step 2: assign delimiter if not yet set
-    // by scanning white spaces in search for the first comma.
-    //
-    // If EOL is reached, comma is chosen to be the delimiter so that
-    // tokens with one KeyVal argument can have spaces around it,
-    // since it is the case when it is obvious there are no other
-    // arguments to parse.
+    final List<TokenWalkInfo> tokens = [];
+
+    /// Step 1: Learn appropriate delimiter by iterating over tokens
+    /// in search for the first comma. [literals] cause the [current]
+    /// index to jump to the matching [Literal.end] character and resumes
+    /// iterating normally.
+    ///
+    /// If EOL is reached, comma is chosen to be the delimiter so that
+    /// tokens with one [KeyVal] argument can have spaces around it,
+    /// since it is the case when it is obvious there are no other
+    /// arguments to parse.
 
     int space = -1, equal = -1;
     int equalCount = 0, spacesBfEq = 0, spacesAfEq = 0;
@@ -207,7 +214,7 @@ class ElementParser {
     bool tokenWalk = false;
     Literal? activeLiteral;
 
-    while (!isDelimiterSet && current < len) {
+    while (current < len) {
       final String c = input[current];
       final bool isComma = Glyphs.comma.char == c;
       final bool isSpace = Glyphs.space.char == c;
@@ -220,7 +227,150 @@ class ElementParser {
           isLiteral = true;
         }
       } else {
+        if (!isSpace && !isEqual) {
+          // The leading equals char determines how the rest of the document
+          // will be parsed when no comma delimiter is set
+          if (!tokenWalk) {
+            (equal == -1) ? tokensBfEq++ : tokensAfEq++;
+          }
+
+          tokenWalk = true;
+          // Clear counted spaces
+          (equal == -1) ? spacesBfEq = 0 : spacesAfEq = 0;
+        } else if (isSpace) {
+          if (tokenWalk) {
+            // Count spaces before and after equals char
+            (equal == -1) ? spacesBfEq++ : spacesAfEq++;
+          }
+          tokenWalk = false;
+
+          if (space == -1) {
+            space = current;
+          }
+        } else if (isEqual) {
+          tokenWalk = false;
+          if (equal == -1) {
+            equal = current;
+          }
+
+          equalCount++;
+        }
+
+        bool continueLoop = false;
+
         // Test all literals to begin a string span
+        for (final Literal literal in udLiterals.keys) {
+          if (literal.begin == c) {
+            isLiteral = true;
+            activeLiteral = literal;
+            udLiterals[activeLiteral] = current;
+
+            current++;
+            continueLoop = true;
+            break;
+          }
+        }
+
+        if (continueLoop) continue;
+      }
+
+      // Ensure literals are terminated before evaluating delimiters.
+      if (isLiteral) {
+        if (udLiterals[activeLiteral] == -1) {
+          udLiterals[activeLiteral!] = current;
+        } else {
+          udLiterals[activeLiteral!] = -1;
+          activeLiteral = null;
+        }
+
+        current++;
+        continue;
+      }
+
+      // Look ahead for terminating literal
+      if ((udLiterals[activeLiteral] ?? -1) != -1) {
+        final int literalEndPos = input.indexOf(activeLiteral!.end, current);
+        if (literalEndPos != -1) {
+          current = literalEndPos;
+          continue;
+        } else {
+          // This loop will never resolve the delimiter because
+          // there is a missing terminating literal.
+          break;
+        }
+      }
+
+      if (isComma) {
+        setDelimiterType(Delimiters.comma);
+        break;
+      }
+
+      current++;
+    }
+
+    // Edge case: one key-value pair can have spaces around them
+    // while being parsed correctly
+    final bool oneTokenExists = equalCount == 1 &&
+        tokensBfEq == 1 &&
+        tokensAfEq <= 1 &&
+        (spacesBfEq - spacesAfEq).abs() <= 1;
+
+    // EOL with no comma delimiter found
+    if (!isDelimiterSet) {
+      // No space token found so there is no other delimiter.
+      // Spaces will be used.
+      if (oneTokenExists && space != -1) {
+        // Step #2 edge case: no delimiter was found
+        // and only **one** key provided, which means
+        // the key-value pair is likely to be surrounded by
+        // whitespace and should be permitted.
+        setDelimiterType(Delimiters.comma);
+      } else {
+        setDelimiterType(Delimiters.space);
+      }
+    }
+
+    // Step 2: Use learned delimiter to collect the tokens
+    current = start;
+    equal = -1;
+    activeLiteral = null;
+    int lastTokenIdx = start;
+
+    // Tokens end on the following delimiter
+    while (current < len) {
+      final String c = input[current];
+      final bool isEqual = Glyphs.equal.char == c;
+      final bool isDelim = delimiter == c;
+
+      bool isLiteral = false;
+      if (activeLiteral != null) {
+        // Test if this is the matching end glyph
+        if (activeLiteral.end == c) {
+          isLiteral = true;
+        }
+      } else {
+        // An equal glyph was found outside a string literal.
+        // Track it to help with token parsing later.
+        if (isEqual) {
+          equal = current;
+          current++;
+          continue;
+        }
+
+        // No active literal span indicates this delimiter is valid
+        if (isDelim) {
+          tokens.add(
+            TokenWalkInfo(
+              data: input.substring(lastTokenIdx, current),
+              pivot: equal - lastTokenIdx,
+            ),
+          );
+          current++;
+          lastTokenIdx = current;
+          continue;
+        }
+
+        // Test all literals to determine if we begin a string span
         for (final Literal literal in udLiterals.keys) {
           if (literal.begin == c) {
             isLiteral = true;
@@ -243,7 +393,7 @@ class ElementParser {
         continue;
       }
 
-      // Look ahead for terminating quote
+      // Look ahead for terminating literal
       if ((udLiterals[activeLiteral] ?? -1) != -1) {
         final int literalEndPos = input.indexOf(activeLiteral!.end, current);
         if (literalEndPos != -1) {
@@ -256,109 +406,48 @@ class ElementParser {
         }
       }
 
-      if (isComma) {
-        setDelimiterType(Delimiters.comma);
-        break;
-      }
-
-      if (!isSpace && !isEqual) {
-        // The leading equals char determines how the rest of the document
-        // will be parsed when no comma delimiter is set
-        if (!tokenWalk) {
-          (equal == -1) ? tokensBfEq++ : tokensAfEq++;
-        }
-
-        tokenWalk = true;
-        // Clear counted spaces
-        (equal == -1) ? spacesBfEq = 0 : spacesAfEq = 0;
-      } else if (isSpace) {
-        if (tokenWalk) {
-          // Count spaces before and after equals char
-          (equal == -1) ? spacesBfEq++ : spacesAfEq++;
-        }
-        tokenWalk = false;
-
-        if (space == -1) {
-          space = current;
-        }
-      } else if (isEqual) {
-        tokenWalk = false;
-        if (equal == -1) {
-          equal = current;
-        }
-
-        equalCount++;
-      }
-
       current++;
     }
 
-    // Edge case: one key-value pair can have spaces around them
-    // while being parsed correctly
-    final bool oneTokenExists = equalCount == 1 &&
-        tokensBfEq == 1 &&
-        tokensAfEq <= 1 &&
-        (spacesBfEq - spacesAfEq).abs() <= 1;
-
-    // EOL with no comma delimiter found
-    if (!isDelimiterSet) {
-      // No space token found so there is no other delimiter.
-      // Spaces will be used.
-      if (space == -1) {
-        return len;
-      } else if (oneTokenExists) {
-        // Step #2 edge case: no delimiter was found
-        // and only **one** key provided, which means
-        // the key-value pair is likely to be surrounded by
-        // whitespace and should be permitted.
-        setDelimiterType(Delimiters.comma);
-      } else {
-        setDelimiterType(Delimiters.space);
-      }
-
-      // Go back to the first space token
-      current = space;
+    // There was a pending token remaining that was not terminated.
+    if (lastTokenIdx < len) {
+      tokens.add(
+        TokenWalkInfo(
+          data: input.substring(lastTokenIdx),
+          pivot: equal - lastTokenIdx,
+        ),
+      );
     }
 
-    // Step 3: use delimiter type to find next end position
-    // which will result in the range [start,end] to be the next token
-    final int idx = input.indexOf(delimiter, start);
-    if (idx == -1) {
-      // Possibly last keyval token. EOL.
-      return len;
-    }
-
-    return min(len, idx);
+    return tokens;
   }
 
-  // TODO: Provide valid equal token pivot
-  void evaluateToken(String input, int start, int end) {
-    // Should never happen
+  void evaluateKeyVals(List<TokenWalkInfo> tokens) {
+    // Sanity check. This should never happen.
     assert(_element != null, 'Element was not initialized.');
 
-    // Trim white spaces around the token for key-val
-    // assignments. e.g. `key=val`
-    final String token = input.substring(start, end).trim();
+    for (final TokenWalkInfo token in tokens) {
+      // Edge case: token is just the equal char
+      // Treat this as no key and no value
+      final TokenWalkInfo(:data, :pivot) = token;
+      if (data == Glyphs.equal.char) continue;
 
-    // Edge case: token is just the equal char
-    // Treat this as no key and no value
-    if (token == Glyphs.equal.char) return;
+      // Trim white spaces around the token for key-val
+      // assignments. e.g. `key=val`
+      if (token.hasPivot) {
+        final KeyVal kv = KeyVal(
+          key: data.substring(0, pivot).trim().unquote().trim(),
+          val: data.substring(pivot + 1, data.length).trim().unquote().trim(),
+        );
 
-    // Named key values are seperated by equal (=) char
-    final int equalPos = token.indexOf(Glyphs.equal.char);
-    if (equalPos != -1) {
-      final KeyVal kv = KeyVal(
-        key: token.substring(0, equalPos).trim().unquote(),
-        val: token.substring(equalPos + 1, token.length).trim().unquote(),
-      );
+        _element?.upsert(kv);
+        continue;
+      }
 
-      _element?.upsert(kv);
-      return;
+      // Nameless key value
+      final KeyVal kv = KeyVal(val: data.trim().unquote().trim());
+      _element?.add(kv);
     }
-
-    // Nameless key value
-    final KeyVal kv = KeyVal(val: token.unquote());
-    _element?.add(kv);
   }
 
   void setOk() {
